81ed796e765a5102e2df9b605c861033cf3727f9
drrozi <drazan.rozic@tu-dortmund.de>
2023-01-11 14:28:17 +0100

    Gonzalez cost added

--- gonzalez.py
+++ gonzalez.py
@@ -3,29 +3,62 @@
 import numpy as np
 import math
 from scipy.spatial import distance
+from my_dists import disc_dtw
 
-def greedygonzalez(data, k, method = 'max'):
-    centers = []
-    cluster_idx = []
-    cluster_idx.append(np.random.randit(o, len(data) - 1))
-    centers.append(data[cluster_idx][0])
+def greedygonzalez(data, k, method = 'euclidean'):
+    centers_idx = []
+    centers_idx.append(np.random.randint(0, len(data) - 1))
 
-    while len(centers) != k:
-        if method == 'max':
-            centers.append(max_dist(data, centers))
-    return centers
+    while len(centers_idx) != k:
+        if method == 'euclidean':
+            centers_idx.append(max_euclidean(data, centers_idx))
+        if method == 'dtw':
+            centers_idx.append(max_dtw(data, centers_idx))
 
-def max_dist(data, centers):
+    return centers_idx
+
+def max_euclidean(data, centers_idx):
     distances = np.zeros(len(data))
 
-    for center_id, center in enumerate(centers):
-        for curve_id, curve in enumerate(data):
-            if distance.euclidean(curve, center) == 0:
-                distances[curve_id] = -math.inf
-            if not math.isinf(distances[curve_id]):
-                distances[curve_id] = distances[curve_id] + distance.euclidean(curve, center)
+    for center_idx in centers_idx:
+        for curve_idx, curve in enumerate(data):
+            if distance.euclidean(curve, data[center_idx]) == 0:
+                distances[curve_idx] = -math.inf
+            if not math.isinf(distances[curve_idx]):
+                distances[curve_idx] = distances[curve_idx] + distance.euclidean(curve, data[center_idx])
 
-    return data[np.argmax(distances)
+    return np.argmax(distances)
 
-                
+def max_dtw(data, centers_idx):
+    distances = np.zeros(len(data))
+
+    for center_idx in centers_idx:
+        for curve_idx, curve in enumerate(data):
+            if disc_dtw(curve, data[center_idx]) == 0:
+                distances[curve_idx] = -math.inf
+            if not math.isinf(distances[curve_idx]):
+                distances[curve_idx] = distances[curve_idx] + disc_dtw(curve, data[center_idx])
+
+    return np.argmax(distances)
+
+
+def gonzalez_cost(data, centers_idx, method = 'euclidean'):
+    cluster_distance = np.full(len(data), np.inf)
+
+    for curve_idx, curve in enumerate(data):
+        for center_idx in centers_idx:
+            if method == 'euclidean':
+                dist = distance.euclidean(curve, data[center_idx])
+            if method == 'dtw':
+                dist = disc_dtw(curve, data[center_idx])
+
+            if cluster_distance[curve_idx] == math.inf:
+                cluster_distance[curve_idx] = dist
+                continue
+            if dist < cluster_distance[curve_idx]:
+                cluster_distance[curve_idx] = dist
+    return np.max(cluster_distance)
+
+
+
 
