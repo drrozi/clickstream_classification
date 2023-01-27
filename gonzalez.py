752aa3b187ba75969354ca17b00b0665b602d567
drrozi <drazan.rozic@tu-dortmund.de>
2023-01-27 15:04:21 +0100

    speedy_gonzalez added

--- gonzalez.py
+++ gonzalez.py
@@ -3,7 +3,8 @@
 import numpy as np
 import math
 from scipy.spatial import distance
-from my_dists import disc_dtw
+from my_dists import disc_dtw, window_ddtw
+from dtaidistance.dtw import distance_matrix
 
 def greedygonzalez(data, k, method = 'euclidean'):
     centers_idx = []
@@ -14,6 +15,8 @@ def greedygonzalez(data, k, method = 'euclidean'):
             centers_idx.append(max_euclidean(data, centers_idx))
         if method == 'dtw':
             centers_idx.append(max_dtw(data, centers_idx))
+        if method == 'wdtw':
+            centers_idx.append(max_wdtw(data, centers_idx))
 
     return centers_idx
 
@@ -41,6 +44,17 @@ def max_dtw(data, centers_idx):
 
     return np.argmax(distances)
 
+def max_wdtw(data, centers_idx):
+    distances = np.zeros(len(data))
+
+    for center_idx in centers_idx:
+        for curve_idx, curve in enumerate(data):
+            if window_ddtw(curve, data[center_idx]) == 0:
+                distances[curve_idx] = -math.inf
+            if not math.isinf(distances[curve_idx]):
+                distances[curve_idx] = distances[curve_idx] + window_ddtw(curve, data[center_idx])
+
+    return np.argmax(distances)
 
 def gonzalez_cost(data, centers_idx, method = 'euclidean'):
     cluster_distance = np.full(len(data), np.inf)
@@ -58,6 +72,30 @@ def gonzalez_cost(data, centers_idx, method = 'euclidean'):
             if dist < cluster_distance[curve_idx]:
                 cluster_distance[curve_idx] = dist
     return np.max(cluster_distance)
+
+def speedy_gonzalez(data, k, window=None, random_state=1971):
+    centers_idx = []
+
+    # Set seed for reproducibility
+    np.random.seed(random_state)
+
+    # First center random
+    centers_idx.append(np.random.randint(0, len(data) +1))
+
+    # Compute disctance matrix with dtaidistance package
+    dm = distance_matrix(data, window=window, parallel=True, use_c=True, only_triu=False)
+
+    # Compute all other k's which are fartest away von actual centroids
+    while len(centers_idx) != k:
+        newcenter_idx = np.argmax(dm[centers_idx].sum(axis=0))
+        centers_idx.append(newcenter_idx)
+
+    return centers_idx
+
+
+
+
+
 
 
 
